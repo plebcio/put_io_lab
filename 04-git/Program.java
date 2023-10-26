9544ff6639b9a1aa6bede7a07d3d4b9dfc47593a
igorwarszawski3 <igorwarszawski3@gmail.com>
2023-10-26 13:53:53 +0200

    Add new java method

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -3,5 +3,11 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    // random java method
+    public static void main2() {
+        System.out.println("Goodby World!");
+    }
+        
 }
 
